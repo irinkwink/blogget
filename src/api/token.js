d76897b23ba4b59addfceb0c1f679371909503a1
irinkwink <irina-ns@yandex.ru>
2022-08-10 09:18:48 +0300

    fix: getToken

--- src/api/token.js
+++ src/api/token.js
@@ -4,14 +4,17 @@ export const setToken = (token) => {
 
 export const getToken = () => {
   let token = '';
+
   if (location.pathname.includes('/auth')) {
     token = new URLSearchParams(location.hash.substring(1))
       .get('access_token');
     setToken(token);
+    return token;
   }
 
   if (localStorage.getItem('bearer')) {
-    setToken(localStorage.getItem('bearer'));
+    token = localStorage.getItem('bearer');
+    setToken(token);
   }
 
   return token;
