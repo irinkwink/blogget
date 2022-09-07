d1ed4357096672cf2aee5c7ff29d4f2bae209e7c
irinkwink <irina-ns@yandex.ru>
2022-09-07 12:08:08 +0300

    fix: const REDIRECT_URI for deploy

--- src/api/const.js
+++ src/api/const.js
@@ -4,5 +4,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'vRByDgsIhRuRXuU8aF3lkg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-irinkwink.vercel.app/auth';
 export const SCOPE = 'identity submit read';
